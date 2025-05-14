f985852ba9ad694d06e25bdf97ae59229b117e5a
IsaiahMitchell <zaymgmt707@gmail.com>
2025-05-13 19:06:53 -0700

    adds starting code for index.js and updates it with new route for App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Components/App';
+import App from '/home/zay/phase-2/task-manager-frontend/src/Components/App.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

73203890e31d6cfc1910bdf933f92ba5aef0837a
IsaiahMitchell <zaymgmt707@gmail.com>
2025-05-13 18:57:25 -0700

    adds starting code for About.js

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>This task manager helps you stay productive and organized.</p>
+    </div>
+  )
+}
+
+export default About
